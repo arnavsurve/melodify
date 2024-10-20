3fe1767d7b9559c4b2e8b2b275505eba0925d0c6
Arnav Surve <arnav@surve.dev>
2024-10-20 00:39:12 -0700

    tempo and notes

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ def hello_world():
 
 @app.route('/midi', methods=["POST"])
 def handle_post_midi():
-    notes = request.json['notes']
-    print(notes)
-    return jsonify({"status": "success", "notes_received": notes}), 200 # TODO: return newly generated midi
+    data = request.get_json()
+    if not data or 'notes' not in data:
+        return jsonify({"status": "error", "message": "Invalid input"}), 400
+
+    notes = data["notes"]
+    tempo = data["tempo"]
+    print(data)
+    return jsonify({"tempo": tempo, "notes": notes}), 200 # TODO: return newly generated midi
 
 if __name__ == "__main__":
     app.run("127.0.0.1", 5001)
